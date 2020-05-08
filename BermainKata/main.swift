fc2f023467a36945e456ca7bd00ae49e4001804c
fikri <fikrihelmi17@gmail.com>
2020-05-08 09:03:09 +0700

    Initial Commit

---
+++ BermainKata/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BermainKata
+//
+//  Created by Fikri on 08/05/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
